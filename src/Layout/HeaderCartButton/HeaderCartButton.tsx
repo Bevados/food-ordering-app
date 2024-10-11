1d537aab0793f02476e7d98c5e8d4515b21c6520
“Vladislav <boromirchik@gmail.com>
2024-10-11 17:07:29 +0300

    added useEffect to cart-button

--- src/Layout/HeaderCartButton/HeaderCartButton.tsx
+++ src/Layout/HeaderCartButton/HeaderCartButton.tsx
@@ -1,18 +1,34 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import CartIcon from '../../Cart/CartIcon'
 import CartContext from '../../store/cart-context/cart-context'
 import styles from './HeaderCartButton.module.css'
 import { HeaderCartButtonProps } from './HeaderCartButton.types'
 
 const HeaderCartButton = ({ onClick }: HeaderCartButtonProps) => {
+	const [isButtonAnimated, setIsButtonAnimated] = useState<boolean>(false)
 	const cartContext = useContext(CartContext)
+
+	const classButton = `${styles.button} ${isButtonAnimated && styles.bump}`
+
+	useEffect(() => {
+		if (cartContext.items.length === 0) {
+			return
+		}
+		setIsButtonAnimated(true)
+
+		const timer = setTimeout(()=> {
+				setIsButtonAnimated(false)
+		}, 300)
+
+		return () => clearTimeout(timer)
+	}, [cartContext.items])
 
 	const cartContextQuantity = cartContext.items.reduce((currentValue, item) => {
 		return currentValue + item.amount
 	}, 0)
 
 	return (
-		<button className={styles.button} onClick={onClick}>
+		<button className={classButton} onClick={onClick}>
 			<span className={styles.icon}>
 				<CartIcon />
 			</span>
